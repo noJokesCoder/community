d3de175f00bf8402bcf5ddf70488da4faf938eee
noJokesCoder <jeka.lyah1992@gmail.com>
2024-12-26 12:07:54 +0100

    Enhance Home page styling and functionality with kitchen menu integration

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { Collapse } from 'antd';
 import { useLoaderData } from 'react-router-dom';
 import { getCmsEntries } from '@/api/fetchFromCMS';
 import { contentTypeEnum, localeTypeEnum } from '@/types/CMS';
+
+// Localization:
+import { localizationDict } from '@/Dict/localization.ts';
 
 import WeatherWidget from '@/components/WeatherWidget';
 
@@ -10,20 +14,67 @@ import useLocaleState from '@/state/locale.ts';
 
 const Home: React.FC = () => {
     let { locale } = useLocaleState();
-    const CMSData = useLoaderData();
+    const { CMShomePage, CMSkitchenMenu } = useLoaderData();
+    const { days, meals } = localizationDict.menu;
+    const currentDay = new Date().toLocaleDateString('en', { weekday: 'long' }).toLowerCase();
+    const weekDayArr = [
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+        'saturday',
+        'sunday',
+    ] as const;
+
+    const getAccordionClassNames = (key: string) => {
+        if (key === currentDay) {
+            return { header: 'isCurrentDay' };
+        }
+        return {
+            header:
+                weekDayArr.indexOf(key as keyof typeof days) < weekDayArr.indexOf(currentDay as keyof typeof days)
+                    ? 'isPastDay'
+                    : 'isFutureDay',
+        };
+    };
 
     if (locale === null) {
         locale = localeTypeEnum.uk;
     }
 
-    console.log('DATA', CMSData);
-    console.log('locale', locale);
+    const pageTitle: string = CMShomePage.title[locale];
+    const heroText: string = CMShomePage.paragraph[locale];
 
-    const pageTitle: string = CMSData.title[locale];
-    const heroText: string = CMSData.paragraph[locale];
+    const menuItems = useMemo(() => {
+        if (!CMSkitchenMenu || CMSkitchenMenu.length === 0) {
+            return [];
+        }
+        return Object.keys(CMSkitchenMenu)
+            .map(key => {
+                if (key !== 'title') {
+                    const [lunch, dinner] = CMSkitchenMenu[key][locale].split('|');
+                    return {
+                        key,
+                        label:
+                            key === currentDay
+                                ? `${days[key as keyof typeof days][locale]} [${new Date().toLocaleDateString('uk')}]`
+                                : days[key as keyof typeof days][locale],
+                        classNames: getAccordionClassNames(key),
+                        children: (
+                            <>
+                                <p className="kitchen__collapse_text">{`${meals.lunch[locale]}: ${lunch}`}</p>
+                                <p className="kitchen__collapse_text">{`${meals.dinner[locale]}: ${dinner}`}</p>
+                            </>
+                        ),
+                    };
+                }
+            })
+            .filter(Boolean);
+    }, [locale, CMSkitchenMenu, currentDay]);
 
     return (
-        <>
+        <div className="home">
             <section className="home_hero">
                 <div className="home_title">
                     <h1>{pageTitle}</h1>
@@ -31,19 +82,41 @@ const Home: React.FC = () => {
                 </div>
             </section>
             <WeatherWidget />
-        </>
+            <section id="kitchenMenu">
+                <h3 className="section-header">{CMSkitchenMenu.title[locale!]}</h3>
+                <Collapse
+                    size="middle"
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-expect-error
+                    items={menuItems}
+                    defaultActiveKey={currentDay}
+                />
+            </section>
+        </div>
     );
 };
 
 export const loader_homePage = async () => {
-    const CMSData = await getCmsEntries({
+    const CMShomePage = await getCmsEntries({
         content_type: contentTypeEnum.homePage,
         // select: 'fields.title',
         // locale: localeTypeEnum.uk,
         locale: '*',
     });
 
-    return CMSData?.length ? CMSData[0].fields : undefined;
+    const CMSkitchenMenu = await getCmsEntries({
+        content_type: contentTypeEnum.kitchenMenu,
+        locale: '*',
+    });
+
+    console.log('KITCHEN_DATA', CMSkitchenMenu);
+
+    return CMShomePage && CMSkitchenMenu
+        ? {
+              CMShomePage: CMShomePage[0].fields,
+              CMSkitchenMenu: CMSkitchenMenu[0].fields,
+          }
+        : undefined;
 };
 
 export default Home;
