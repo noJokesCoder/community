e6f0ba669f6243c130bac99268e2129335b63194
noJokesCoder <jeka.lyah1992@gmail.com>
2024-12-28 11:18:39 +0100

    Update ESLint configuration and dependencies for improved React support

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
 
 export default tseslint.config(
-    { ignores: ['dist'] },
+    { ignores: ['dist'], settings: { react: { version: 'detect' } } },
     {
         extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
         files: ['**/*.{ts,tsx}'],
@@ -15,10 +16,13 @@ export default tseslint.config(
             globals: globals.browser,
         },
         plugins: {
+            react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
         },
         rules: {
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
         },
