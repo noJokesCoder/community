e0c4ce936569f286a1ceefdbd8cfd06fba69e89e
noJokesCoder <jeka.lyah1992@gmail.com>
2024-12-28 10:35:12 +0100

    Add custom hook  useIntersectionObserver

---
+++ src/hooks/useIntersectionObserver/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+export const useIntersectionObserver = (ref: React.RefObject<Element>) => {
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const intersectionCb = (entries: any) => {
+        const [entry] = entries;
+
+        setIsVisible(entry.isIntersecting);
+    };
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(intersectionCb, {
+            root: null,
+            rootMargin: '90px',
+            threshold: 1.0,
+        });
+        let watchedNode: Element | null = null;
+        if (ref.current) {
+            watchedNode = ref.current;
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            if (watchedNode) {
+                observer.unobserve(watchedNode);
+            }
+        };
+    }, [ref]);
+
+    return isVisible;
+};
