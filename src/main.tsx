94086aad157267ef33314bcef0180e8cca0db676
noJokesCoder <jeka.lyah1992@gmail.com>
2025-01-03 16:31:37 +0100

    Add locale & theme-config to AntD

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// AntD:
+import { ConfigProvider, theme } from 'antd';
+import en from 'antd/locale/en_US';
+import ua from 'antd/locale/uk_UA';
+// State:
+import useLocaleState from '@/state/locale';
+
+import { localeTypeEnum } from '@/types/CMS';
 import './index.scss';
 
 import Router from './router';
@@ -10,7 +18,20 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <Router />
+            <ConfigProvider
+                locale={useLocaleState.getState().locale === localeTypeEnum.uk ? ua : en}
+                theme={{
+                    token: {
+                        ...theme.getDesignToken(),
+                        colorPrimary: '#0b96d1',
+                        colorTextBase: '#191919',
+                        colorInfo: '#0b96d1',
+                        colorLink: '#0b96d1',
+                    },
+                }}
+            >
+                <Router />
+            </ConfigProvider>
         </QueryClientProvider>
     </StrictMode>
 );
